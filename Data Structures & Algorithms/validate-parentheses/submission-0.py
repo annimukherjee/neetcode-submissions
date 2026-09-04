7eb14c1fd8803b696305ef8dc8b30c9750cb47ad
Aniruddha Mukherjee <mukh.aniruddha@gmail.com>
2026-09-04 04:10:30 +0200

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        l = []
+        for bracket in s:
+            print(bracket)
+            if bracket in "({[":
+                # print("in-if")
+                l.append(bracket)
+            elif bracket in ")}]":
+                # print("in elif")
+                if (bracket == ")" and l[-1] == "(") or (bracket == "}" and l[-1] == "{") or (bracket == "]" and l[-1] == "["):
+                    l.pop()
+        
+        if l == []:
+            return True
+        else:
+            return False
